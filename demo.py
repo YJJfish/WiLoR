fbcfab5e62238f0f23b69a517cfab5c99646bea1
YJJfish <jinjiany@andrew.cmu.edu>
2024-12-10 19:27:26 -0500

    export masks and meshes with arms

--- demo.py
+++ demo.py
@@ -16,9 +16,10 @@ LIGHT_PURPLE=(0.25098039,  0.274117647,  0.65882353)
 
 def main():
     parser = argparse.ArgumentParser(description='WiLoR demo code')
-    parser.add_argument('--img_folder', type=str, default='images', help='Folder with input images')
-    parser.add_argument('--out_folder', type=str, default='out_demo', help='Output folder to save rendered results')
-    parser.add_argument('--save_mesh', dest='save_mesh', action='store_true', default=False, help='If set, save meshes to disk also')
+    parser.add_argument('--img_folder', type=str, default='demo_img', help='Folder with input images')
+    parser.add_argument('--overlay_folder', type=str, default='overlay_out', help='Overlay folder to save rendered results')
+    parser.add_argument('--mesh_folder', type=str, default='mesh_out', help='Mesh folder to save mesh results')
+    parser.add_argument('--param_folder', type=str, default='param_out', help='Parameter folder to save MANO parameters')
     parser.add_argument('--rescale_factor', type=float, default=2.0, help='Factor for padding the bbox')
     parser.add_argument('--file_type', nargs='+', default=['*.jpg', '*.png', '*.jpeg'], help='List of file extensions to consider')
 
@@ -37,13 +38,19 @@ def main():
     model.eval()
 
     # Make output directory if it does not exist
-    os.makedirs(args.out_folder, exist_ok=True)
+    os.makedirs(args.overlay_folder, exist_ok=True)
+    os.makedirs(args.mesh_folder, exist_ok=True)
+    os.makedirs(args.param_folder, exist_ok=True)
 
     # Get all demo images ends with .jpg or .png
     img_paths = [img for end in args.file_type for img in Path(args.img_folder).glob(end)]
     # Iterate over all images in folder
     for img_path in img_paths:
+        print(f"==== {img_path} ====", flush=True)
         img_cv2 = cv2.imread(str(img_path))
+        if img_cv2 is None:
+            print("None encountered. Skip.", flush=True)
+            continue
         detections = detector(img_cv2, conf = 0.3, verbose=False)[0]
         bboxes    = []
         is_right  = []
@@ -60,10 +67,14 @@ def main():
         dataloader = torch.utils.data.DataLoader(dataset, batch_size=16, shuffle=False, num_workers=0)
 
         all_verts = []
+        all_verts_with_arm = []
         all_cam_t = []
         all_right = []
         all_joints= []
         all_kpts  = []
+        all_global_orient = []
+        all_hand_pose = []
+        all_betas = []
         
         for batch in dataloader: 
             batch = recursive_to(batch, device)
@@ -88,26 +99,34 @@ def main():
                 img_fn, _ = os.path.splitext(os.path.basename(img_path))
                 
                 verts  = out['pred_vertices'][n].detach().cpu().numpy()
+                verts_with_arm  = out['pred_vertices_with_arm'][n].detach().cpu().numpy()
                 joints = out['pred_keypoints_3d'][n].detach().cpu().numpy()
                 
                 is_right    = batch['right'][n].cpu().numpy()
                 verts[:,0]  = (2*is_right-1)*verts[:,0]
+                verts_with_arm[:,0]  = (2*is_right-1)*verts_with_arm[:,0]
                 joints[:,0] = (2*is_right-1)*joints[:,0]
                 cam_t = pred_cam_t_full[n]
                 kpts_2d = project_full_img(verts, cam_t, scaled_focal_length, img_size[n])
                 
                 all_verts.append(verts)
+                all_verts_with_arm.append(verts_with_arm)
                 all_cam_t.append(cam_t)
                 all_right.append(is_right)
                 all_joints.append(joints)
                 all_kpts.append(kpts_2d)
-                
+                all_global_orient.append(out["pred_mano_params"]["global_orient"][n].detach().cpu().numpy())
+                all_hand_pose.append(out["pred_mano_params"]["hand_pose"][n].detach().cpu().numpy())
+                all_betas.append(out["pred_mano_params"]["betas"][n].detach().cpu().numpy())
                 
                 # Save all meshes to disk
-                if args.save_mesh:
-                    camera_translation = cam_t.copy()
-                    tmesh = renderer.vertices_to_trimesh(verts, camera_translation, LIGHT_PURPLE, is_right=is_right)
-                    tmesh.export(os.path.join(args.out_folder, f'{img_fn}_{n}.obj'))
+                camera_translation = cam_t.copy()
+                tmesh = renderer.vertices_to_trimesh(verts, camera_translation, LIGHT_PURPLE, is_right=is_right)
+                tmesh.export(os.path.join(args.mesh_folder, f'{img_fn}_{n}.obj'))
+
+                camera_translation = cam_t.copy()
+                tmesh = renderer.vertices_to_trimesh(verts_with_arm, camera_translation, LIGHT_PURPLE, is_right=is_right)
+                tmesh.export(os.path.join(args.mesh_folder, f'{img_fn}_{n}_arm.obj'))
 
         # Render front view
         if len(all_verts) > 0:
@@ -116,14 +135,31 @@ def main():
                 scene_bg_color=(1, 1, 1),
                 focal_length=scaled_focal_length,
             )
-            cam_view = renderer.render_rgba_multiple(all_verts, cam_t=all_cam_t, render_res=img_size[n], is_right=all_right, **misc_args)
 
             # Overlay image
+            cam_view = renderer.render_rgba_multiple(all_verts, cam_t=all_cam_t, render_res=img_size[n], is_right=all_right, **misc_args)
             input_img = img_cv2.astype(np.float32)[:,:,::-1]/255.0
             input_img = np.concatenate([input_img, np.ones_like(input_img[:,:,:1])], axis=2) # Add alpha channel
             input_img_overlay = input_img[:,:,:3] * (1-cam_view[:,:,3:]) + cam_view[:,:,:3] * cam_view[:,:,3:]
 
-            cv2.imwrite(os.path.join(args.out_folder, f'{img_fn}.jpg'), 255*input_img_overlay[:, :, ::-1])
+            # Render masks
+            mask = cam_view[:, :, [3, 3, 3]]
+            cam_view = renderer.render_rgba_multiple(all_verts_with_arm, cam_t=all_cam_t, render_res=img_size[n], is_right=all_right, **misc_args)
+            mask_arm = cam_view[:, :, [3, 3, 3]]
+
+            cv2.imwrite(os.path.join(args.overlay_folder, f'{img_fn}.jpg'), 255*input_img_overlay[:, :, ::-1])
+            cv2.imwrite(os.path.join(args.overlay_folder, f'{img_fn}_mask.jpg'), 255*mask)
+            cv2.imwrite(os.path.join(args.overlay_folder, f'{img_fn}_mask_arm.jpg'), 255*mask_arm)
+
+            # Save parameters
+            np.savez(
+                os.path.join(args.param_folder, f'{img_fn}_params.npz'),
+                cam=np.stack(all_cam_t, axis=0),
+                global_orient=np.stack(all_global_orient, axis=0),
+                hand_pose=np.stack(all_hand_pose, axis=0),
+                betas=np.stack(all_betas, axis=0)
+            )
+
 
 def project_full_img(points, cam_trans, focal_length, img_res): 
     camera_center = [img_res[0] / 2., img_res[1] / 2.]
